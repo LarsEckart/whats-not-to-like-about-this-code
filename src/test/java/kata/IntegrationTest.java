f9f68b72672812fe54c8b653fc59dd5f48bef1b1
Lars Eckart <lars.eckart@hey.com>
2025-04-10 09:20:54 +0300

    verify 2 emails sent

--- src/test/java/kata/IntegrationTest.java
+++ src/test/java/kata/IntegrationTest.java
@@ -41,17 +41,11 @@ class IntegrationTest {
 
     @Test
     void it_works() {
-        String newDeliveryJson = """
-                {
-                  "email": "test@example.com",
-                  "latitude": 58.377065,
-                  "longitude": 26.727897
-                }""";
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        HttpEntity<String> newDeliveryRequest = new HttpEntity<>(newDeliveryJson, headers);
-        restTemplate.exchange("/delivery/new", HttpMethod.POST, newDeliveryRequest, String.class);
+        registerNewDelivery(headers, "test@example.com");
+        registerNewDelivery(headers, "test2@example.com");
 
         String deliveryJson = """
                 {
@@ -64,10 +58,27 @@ class IntegrationTest {
         restTemplate.exchange("/delivery/update", HttpMethod.POST, deliveryRequest, String.class);
 
         // Verify the email sent
-        assertThat(emailGateway.getLastRecipient()).isEqualTo("test@example.com");
-        assertThat(emailGateway.getLastSubject()).isNotNull();
-        assertThat(emailGateway.getLastMessage()).contains("Regarding your delivery today at ", "How likely would you be to recommend this delivery service to a friend?");
+        NoOpEmailGateway.Email lastEmail = emailGateway.getLastEmail();
+        assertThat(lastEmail.recipient()).isEqualTo("test2@example.com");
+        assertThat(lastEmail.subject()).isEqualTo("Your delivery will arrive soon");
+        assertThat(lastEmail.message()).contains("Your delivery to ", " is next, estimated time of arrival is in 0 minutes. Be ready!");
 
+        // Verify the feedback email is sent
+        assertThat(emailGateway.getSentEmails().get(0).recipient()).isEqualTo("test@example.com");
+        assertThat(emailGateway.getSentEmails().get(0).subject()).isEqualTo("Your feedback is important to us");
+        assertThat(emailGateway.getSentEmails().get(0).message()).contains("Regarding your delivery today at ", "How likely would you be to recommend this delivery service to a friend?");
+    }
+
+    private void registerNewDelivery(HttpHeaders headers, String mail) {
+        String newDeliveryJson = """
+                {
+                  "email": "%s",
+                  "latitude": 58.377065,
+                  "longitude": 26.727897
+                }""".formatted(mail);
+
+        HttpEntity<String> newDeliveryRequest = new HttpEntity<>(newDeliveryJson, headers);
+        restTemplate.exchange("/delivery/new", HttpMethod.POST, newDeliveryRequest, String.class);
     }
 
     @TestConfiguration
--- src/test/java/kata/NoOpEmailGateway.java
+++ src/test/java/kata/NoOpEmailGateway.java
@@ -2,33 +2,31 @@ package kata;
 
 import org.slf4j.Logger;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.slf4j.LoggerFactory.getLogger;
 
 class NoOpEmailGateway implements EmailGateway {
 
     private static final Logger log = getLogger(NoOpEmailGateway.class);
 
-    private String lastRecipient;
-    private String lastSubject;
-    private String lastMessage;
+    private final List<Email> sentEmails = new ArrayList<>();
 
     @Override
     public void send(String to, String s, String m) {
         log.info("Sending email with subject \"{}\" to {} with message \"{}\"", s, to, m);
-        this.lastRecipient = to;
-        this.lastSubject = s;
-        this.lastMessage = m;
+        sentEmails.add(new Email(to, s, m));
     }
 
-    public String getLastRecipient() {
-        return lastRecipient;
+    public List<Email> getSentEmails() {
+        return Collections.unmodifiableList(sentEmails);
     }
 
-    public String getLastSubject() {
-        return lastSubject;
+    public Email getLastEmail() {
+        return sentEmails.isEmpty() ? null : sentEmails.get(sentEmails.size() - 1);
     }
 
-    public String getLastMessage() {
-        return lastMessage;
-    }
+    public record Email(String recipient, String subject, String message) {}
 }
